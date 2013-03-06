6e57ed83c98b09290a78ffa5510ec875b7b3ca73
Henrik IR <hiringel@gmail.com>
2013-03-06 12:02:51 +0100

    Commit 2
    KartActivity og button fra MainActivity til KartActivity, kommentert ut
    onCreateOptionsMenu

---
+++ src/com/example/norserun/KartActivity.java
@@ -0,0 +1,61 @@
+package com.example.norserun;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import no.nkgs.webatlas.android.WALayer;
+import no.nkgs.webatlas.android.WAMapStyle;
+import no.nkgs.webatlas.android.WAMapView;
+import no.nkgs.webatlas.android.WASettings;
+
+
+public class KartActivity extends Activity {
+
+	WAMapView mapView;
+	WALayer layer;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kartet);
+		mapView = (WAMapView) findViewById(R.id.kartet);
+        WASettings.getInstance().setTilesFadeIn(true);    //Denne er valgfri men visuelt penere
+        //      Init mapView
+        layer = new WALayer(WAMapStyle.VECTOR);
+        mapView.addLayer(layer);					//Du må ha et "filter" for at mappet ditt skal vises på skjermen
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+ 
+		mapView.resume();
+	}
+ 
+	@Override
+	protected void onPause() {
+		super.onPause();
+ 
+		mapView.pause();
+	}
+ 
+	@Override
+	protected void onStop() {
+		super.onStop();
+ 
+	}
+ 
+	@Override
+	protected void onDestroy() {
+		mapView.dispose();
+ 
+		super.onDestroy();
+	}
+}
--- src/com/example/norserun/MainActivity.java
+++ src/com/example/norserun/MainActivity.java
@@ -1,22 +1,52 @@
 package com.example.norserun;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends Activity {
 
+	//Instansiere Button widget
+	Button kartButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//Button-ting, lage button og lage listener
+		addListenerOnButton();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	//Dette er for menu-knappen
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.activity_main, menu);
+//		return true;
+//	}
+	
+	public void addListenerOnButton() {
+		 
+		final Context context = this;
+ 
+		kartButton = (Button) findViewById(R.id.kartButton);
+ 
+		kartButton.setOnClickListener(new OnClickListener() {
+ 
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, KartActivity.class);
+                            startActivity(intent);   
+ 
+			}
+ 
+		});
 	}
-
+	
+	
 }
