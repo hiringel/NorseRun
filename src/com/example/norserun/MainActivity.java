4c646e3d03b8d1b46a84990c312a8f6e59aaa29d
Dzenan Dumpor <dumpor@gmail.com>
2013-03-14 11:09:17 +0100

    Oppdaterte trip class
    La til time og arraylist

--- src/com/example/norserun/MainActivity.java
+++ src/com/example/norserun/MainActivity.java
@@ -1,44 +1,28 @@
 package com.example.norserun;
 
-import java.util.HashMap;
-
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.ActionBar.TabListener;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.SubMenu;
 
 
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TabHost;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 
-public class MainActivity extends SherlockFragmentActivity implements ActionBar.TabListener{
+public class MainActivity extends SherlockActivity implements ActionBar.TabListener {
 
 	//Instansiere Button widget
 	Button kartButton;
-	HomeFragment homeFrag;
-	StatisticsFragment statFrag;
-	LastTripsFragment tripFrag;
-
-	
-
-
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +31,11 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 		
 		//Lager tabsa
 		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-
 		initializeActionBarTabs();
-		homeFrag = null;
-		statFrag = null;
-		tripFrag = null;
-
-		
-		
-		
 		
 		//Button-ting, lage button og lage listener
-		//addListenerOnButton();
+		addListenerOnButton();
 	}
-	
-
 
 
 
@@ -94,9 +67,6 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 		homeTab.setText(this.getString(R.string.homeTabString));
 		lastTripsTab.setText(this.getString(R.string.lastTripsTabString));
 		statisticsTab.setText(this.getString(R.string.statisticTabString));
-		homeTab.setTag(1);
-		lastTripsTab.setTag(2);
-		statisticsTab.setTag(3);
 		homeTab.setTabListener(this);
 		lastTripsTab.setTabListener(this);
 		statisticsTab.setTabListener(this);
@@ -107,104 +77,42 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 	
 
 
-
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
 		
-	
+	}
 
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
 
-	
-	
-//	public void addListenerOnButton() {
-//		 
-//		final Context context = this;
-// 
-//		kartButton = (Button) findViewById(R.id.kartButton);
-// 
-//		kartButton.setOnClickListener(new OnClickListener() {
-// 
-//			public void onClick(View arg0) {
-// 
-//			    Intent intent = new Intent(context, KartActivity.class);
-//                            startActivity(intent);   
-// 
-//			}
-// 
-//		});
-//	}
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-@Override
-public void onTabSelected(Tab tab, FragmentTransaction ft) {
-	
-	switch(Integer.parseInt(tab.getTag().toString())){
-		case 1: 
-			if(homeFrag == null){
-				homeFrag = new HomeFragment();
-				getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, homeFrag).commit();
-				Log.d("Fragment", "Added home!");
-			}
-			else
-			{
-			getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, homeFrag).commit();
-			Log.d("Fragment", "Replaced to home");
-			}
-			
-			break;
-		case 2:
-			if(tripFrag == null){
-				tripFrag = new LastTripsFragment();
-				getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, tripFrag).commit();
-				Log.d("Fragment", "Added trip");
-			}
-			else{
-				getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, tripFrag).commit();
-				Log.d("Fragment", "Replaced to trip");
-			}
-
-			break;
-		case 3:
-			if(statFrag == null){
-				statFrag = new StatisticsFragment();
-				getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, statFrag).commit();
-				Log.d("Fragment", "Added stat");
-			}
-			else{
-				getSupportFragmentManager().beginTransaction().replace(R.id.realtabcontent, statFrag).commit();
-				Log.d("Fragment", "Replaced to stat");
-			}
-	
-			break;
-		default:
-			break;
-			
-
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-
-}
-
-@Override
-public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 	
-	// TODO Auto-generated method stub
+	public void addListenerOnButton() {
+		 
+		final Context context = this;
+ 
+		kartButton = (Button) findViewById(R.id.kartButton);
+ 
+		kartButton.setOnClickListener(new OnClickListener() {
+ 
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, KartActivity.class);
+                            startActivity(intent);   
+ 
+			}
+ 
+		});
+	}
 	
-}
-
-@Override
-public void onTabReselected(Tab tab, FragmentTransaction ft) {
-	// TODO Auto-generated method stub
 	
-}
 }
--- src/com/example/norserun/Trip.java
+++ src/com/example/norserun/Trip.java
@@ -1,15 +1,18 @@
 package com.example.norserun;
+import android.location.*;
+import android.text.format.Time;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Trip {
 	
 	private String name;
 	private String tripType;
-	private String date; //<-- Neppe string, men Dateobjekt, se om det finnes noen greie ferdiglagde som kan brukes, ala jodatime.
+	private Time date; //<-- Neppe string, men Dateobjekt, se om det finnes noen greie ferdiglagde som kan brukes, ala jodatime.
 	private double distance;
 	
-	private List[] geoPointArray; //<-- Kan være du kan bytte ut List med noe mer brukende!
+	private List<Location> geoPointArray; //<-- Kan være du kan bytte ut List med noe mer brukende!
 	
 	//Ting som er avhengige av en timer
 	private double avgSpeed;
@@ -18,10 +21,14 @@ public class Trip {
 	public Trip(String name, String tripType){
 		this.name = name;
 		this.tripType = tripType;
+		
 		//Setdate med en funskjon
+		
 		this.distance = 0;
 		this.avgSpeed = 0;
 		this.duration = 0;
+		
+		this.geoPointArray = new ArrayList<Location>();
 	}
 	
 	
